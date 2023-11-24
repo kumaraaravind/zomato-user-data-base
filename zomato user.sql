3d2356e320609bc10d26f48ff17269529b82e39b
kumaraaravind <126396194+kumaraaravind@users.noreply.github.com>
2023-11-24 10:00:25 +0530

    Add files via upload

---
+++ zomato user.sql
@@ -0,0 +1,14 @@
+use zomato;
+create table zomatouser(
+id int,
+name varchar(45),
+email varchar(45),
+phonenumber  int,
+location  varchar(45),
+address  varchar(45),
+street  varchar(45),
+city  varchar(15),
+state  varchar(40),
+county  varchar(10),
+pincode int
+);
